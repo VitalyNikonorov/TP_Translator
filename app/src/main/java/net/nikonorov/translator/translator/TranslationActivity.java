358d94584c8f71bc2593ec676165aca5c99bbbf2
Виталий Никоноров <vitaly@MacBook-Pro-Vitalij.local>
2015-09-17 23:09:13 +0300

    three activities

---
+++ app/src/main/java/net/nikonorov/translator/translator/TranslationActivity.java
@@ -0,0 +1,129 @@
+package net.nikonorov.translator.translator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+public class TranslationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translation);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            initNavigationDrawer(toolbar);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_translation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initNavigationDrawer(Toolbar toolbar){
+
+
+        AccountHeader accountHeader = initAccountHeader();
+
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withIcon(R.drawable.ic_translate_black_36dp)
+                                .withName(R.string.nav_menu_item1)
+                                .withIdentifier(1)
+                                .withSetSelected(true),
+                        new DividerDrawerItem(),
+
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item2)
+                                .withIcon(R.drawable.ic_settings_applications_black_36dp)
+                                .withIdentifier(2)
+                                .withSetSelected(false)
+                        ,
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item3)
+                                .withIcon(R.drawable.ic_info_outline_black_36dp)
+                                .withIdentifier(3)
+                                .withSetSelected(false)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int i, IDrawerItem iDrawerItem) {
+                        Intent intent;
+                        switch (iDrawerItem.getIdentifier()) {
+                            case 2:
+                                intent = new Intent(TranslationActivity.this, SettingsActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 3:
+                                intent = new Intent(TranslationActivity.this, AboutUsActivity.class);
+                                startActivity(intent);
+                                break;
+                        }
+                        return false;
+                    }
+                })
+                .withAccountHeader(accountHeader)
+                .build();
+    }
+
+    private AccountHeader initAccountHeader(){
+
+        IProfile profile = new ProfileDrawerItem()
+                .withEmail("email")
+                .withName("Your Name")
+                .withIcon(R.drawable.ic_verified_user_black_36dp);
+
+        return new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.md_nd_background)
+                .addProfiles(profile)
+                .build();
+
+    }
+
+
+}
